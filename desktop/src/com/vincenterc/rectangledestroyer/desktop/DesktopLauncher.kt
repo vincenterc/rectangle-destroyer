2c1ba673760c770089606f32ce91c461bcb37d83
vincenterc <chung.menghsun@gmail.com>
2020-09-25 18:48:42 +0800

    Init

---
+++ desktop/src/com/vincenterc/rectangledestroyer/desktop/DesktopLauncher.kt
@@ -0,0 +1,13 @@
+package com.vincenterc.rectangledestroyer.desktop
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.vincenterc.rectangledestroyer.RectangleGame
+
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = LwjglApplicationConfiguration()
+        LwjglApplication(RectangleGame(), config)
+    }
+}
